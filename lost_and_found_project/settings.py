37ab35d4520a6ff2b800f54d038cabffb301d572
Advait Razdan <djhamburger@10.0.6.38>
2026-02-06 23:48:34 +0530

    more changes to settings.py and requirements.txt

--- lost_and_found_project/settings.py
+++ lost_and_found_project/settings.py
@@ -55,10 +55,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "lost_and_found_project.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
